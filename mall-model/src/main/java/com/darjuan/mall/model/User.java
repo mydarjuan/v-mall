098a580fbf6ba9f9fb3926b00ef44758590afc11
liujianbo <liujianbo@kuainiujinke.com>
2020-02-28 13:05:27 +0800

    添加skywalking

--- mall-model/src/main/java/com/darjuan/mall/model/User.java
+++ mall-model/src/main/java/com/darjuan/mall/model/User.java
@@ -1,7 +1,8 @@
 package com.darjuan.mall.model;
 
-import javax.validation.constraints.NotBlank;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author 刘建波
@@ -10,14 +11,14 @@ import lombok.Data;
 @Data
 public class User {
 
-  private String id;
+    private String id;
 
-  @NotBlank(message = "姓名不能为空")
-  private String name;
+    @NotBlank(message = "姓名不能为空")
+    private String name;
 
-  @NotBlank(message = "年龄不能为空")
-  private Integer age;
+    @NotBlank(message = "年龄不能为空")
+    private Integer age;
 
-  @NotBlank(message = "邮件不能为空")
-  private String email;
+    @NotBlank(message = "邮件不能为空")
+    private String email;
 }
